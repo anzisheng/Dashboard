0c7d7c5ed82c4907d9de8345d7480f6936520056
anzisheng <anzisheng@163.com>
2026-06-16 16:39:07 +0800

    mmx 界面已显示

--- Dialog3.cpp
+++ Dialog3.cpp
@@ -9,18 +9,30 @@
 Dialog3::Dialog3(QWidget* parent)
     : QDialog(parent)
 {
-    setWindowTitle(QStringLiteral("对话框 3"));
-    setMinimumSize(300, 150);
+    setWindowTitle(QStringLiteral("MMX 输入"));
+    setMinimumSize(300, 250);
 
     auto* layout = new QVBoxLayout(this);
 
-    auto* label = new QLabel(QStringLiteral("这是第三个对话框"), this);
+    auto* label = new QLabel(QStringLiteral("请选择MMX输入"), this);
     label->setAlignment(Qt::AlignCenter);
     layout->addWidget(label);
 
-    m_lineEdit = new QLineEdit(this);
-    m_lineEdit->setPlaceholderText(QStringLiteral("请输入内容..."));
-    layout->addWidget(m_lineEdit);
+    m_bCheckStopIn = new QCheckBox("急停",this);
+    //m_lineEdit->setPlaceholderText(QStringLiteral("请输入内容..."));
+    layout->addWidget(m_bCheckStopIn);
+
+    m_bCheckOverPressure = new QCheckBox("过压报警", this);// nullptr;//过压报警
+    layout->addWidget(m_bCheckOverPressure);
+
+     m_bCheckRelief = new QCheckBox("泄压阀", this);// nullptr;//泄压阀
+    layout->addWidget(m_bCheckRelief);
+
+    m_bCheckBeep = new QCheckBox("蜂鸣器", this);// nullptr;//泄压阀
+    layout->addWidget(m_bCheckBeep);
+
+    m_bCheckPump = new QCheckBox("补油泵", this);// nullptr;//泄压阀
+	layout->addWidget(m_bCheckPump);
 
     m_okButton = new QPushButton(QStringLiteral("确定"), this);
     layout->addWidget(m_okButton);
--- Dialog3.h
+++ Dialog3.h
@@ -2,9 +2,11 @@
 #define DIALOG3_H
 
 #include <QDialog>
+#include <QCheckBox>
 
 class QLineEdit;
 class QPushButton;
+class QCheckBox;
 
 class Dialog3 : public QDialog
 {
@@ -17,7 +19,15 @@ public:
 private slots:
     void onOkClicked();
 
-private:
+public:
+    QCheckBox* m_bCheckStopIn = nullptr;//¼±Í£
+    QCheckBox* m_bCheckOverPressure = nullptr;//¹ýÑ¹±¨¾¯
+    QCheckBox* m_bCheckRelief = nullptr;//Ð¹Ñ¹·§
+    QCheckBox* m_bCheckBeep = nullptr;//·äÃùÆ÷
+        
+    QCheckBox* m_bCheckPump = nullptr;//²¹ÓÍ±Ã
+   
+
     QLineEdit* m_lineEdit = nullptr;
     QPushButton* m_okButton = nullptr;
 };
