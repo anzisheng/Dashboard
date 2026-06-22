fd462e5ac042cb508f281720b1043969efcad892
anzisheng <anzisheng@163.com>
2026-06-22 11:24:00 +0800

    固定大小

--- main.cpp
+++ main.cpp
@@ -6,7 +6,9 @@ int main(int argc, char* argv[])
     QApplication app(argc, argv);
     MainWindow window;
     window.resize(800, 600);
-    window.setWindowTitle("Dashboard Demo");
+    window.setWindowTitle("火主机控制系统");
+
+    window.setWindowState(window.windowState() ^ Qt::WindowFullScreen);
     window.show();
     return app.exec();
 }
